8249c47300e0fd1ef5ca14941f9331586c32597b
Abraar117 <kaziabraar@gmail.com>
2022-09-29 12:57:28 +0530

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Student from './components/Student.js';
 import Teacher from './components/Teacher.js';
-import navbar from './components/navbar.js';
+import Navigation from './components/Navigation.js';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <navbar />
+      <Navigation />
       <Student />
       <Teacher />
     </div>
