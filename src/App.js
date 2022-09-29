d194f70b56a2b34aeed4399c62a4e2af1cbc70a7
Abraar117 <kaziabraar@gmail.com>
2022-09-29 12:53:50 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Student from './components/Student.js';
 import Teacher from './components/Teacher.js';
+import navbar from './components/navbar.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <navbar />
       <Student />
       <Teacher />
     </div>
